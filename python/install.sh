544b30d971c4bb9ef3ea390aa0ea526c24a4e649
Frode Egeland <frode.egeland@fairfaxmedia.com.au>
2020-06-25 22:45:04 +1000

    add python setup
    Signed-off-by: Frode Egeland <frode.egeland@fairfaxmedia.com.au>

---
+++ python/install.sh
@@ -0,0 +1,12 @@
+#!/usr/local/bin/bash
+
+PY_VENV=~/py3
+if [ ! -d "$PY_VENV" ]; then
+    HB_PYTHON_DIR=$(ls -d /usr/local/Cellar/python* | sort | tail -n 1)
+    PYTHON3=$(ls $HB_PYTHON_DIR/*/bin/python3)
+    $PYTHON3 -m venv "$PY_VENV"
+    source ${PY_VENV}/bin/activate
+    pip install --upgrade --progress-bar pretty pip
+else
+    echo "Python 3 venv (${PY_VENV}) exists"
+fi
