cc25da6dfe9684d1eae052fb5eeebe16d632d657
Frode Egeland <frode.egeland@fairfaxmedia.com.au>
2020-06-26 00:01:45 +1000

    trust the force - brew bundle cleanup --force, that is
    Signed-off-by: Frode Egeland <frode.egeland@fairfaxmedia.com.au>

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -6,6 +6,6 @@ brew update
 brew tap homebrew/bundle
 brew upgrade
 brew bundle
-brew bundle cleanup --verbose && echo "To clean up, run: brew bundle cleanup --file $(pwd)/Brewfile --force"
+brew bundle cleanup --verbose --force
 
 cd ..
