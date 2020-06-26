6431f73e0eff9a786bddb06b9cc9d9bf0d26b02b
Frode Egeland <frode.egeland@fairfaxmedia.com.au>
2020-06-26 21:11:21 +1000

    tweaks
    Signed-off-by: Frode Egeland <frode.egeland@fairfaxmedia.com.au>

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,5 @@
 
 # Use starship for prompt magic
 starship init fish | source
+
+source ~/py3/bin/activate.fish
---
+++ gnupg/install.sh
@@ -0,0 +1,16 @@
+#!/usr/local/bin/bash
+
+echo -n "Bitwarden login: "
+bw login --check || bw login
+
+until bw unlock --check
+do
+    echo "Enter Master Password: "
+    export BW_SESSION=$(bw --raw unlock)
+done
+
+# export GPG_FOLDER_ID=$(bw list folders --search GPG | jq -r '.[0].id')
+
+DECODE_PASSWORD=$(bw get item "87604617-5c5e-4af6-83aa-abe600907a16" | jq -r '.notes')
+
+gpg --passphrase "${DECODE_PASSWORD}" --batch --no-use-agent --decrypt secret_keys.enc | gpg --import
