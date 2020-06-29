bbc2ca1d935fa667b0979dec41739269f5e6278c
Frode Egeland <frode.egeland@fairfaxmedia.com.au>
2020-06-29 14:35:29 +1000

    add fisher for fish shell
    Signed-off-by: Frode Egeland <frode.egeland@fairfaxmedia.com.au>

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -11,3 +11,10 @@ set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -x AWS_REGION "ap-southeast-2"
 set -x AWS_PROFILE "saml"
 set -x AWS_DEFAULT_PROFILE "saml"
+
+# Set up fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
