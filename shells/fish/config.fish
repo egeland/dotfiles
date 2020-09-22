3a3e1d585fa10525aec58541cb0953e77ddb3dd8
Frode Egeland <egeland@gmail.com>
2020-09-22 22:26:22 +1000

    [linux] ensure we get gpg agent prompt in right place
    Signed-off-by: Frode Egeland <egeland@gmail.com>

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -31,4 +31,5 @@ zoxide init fish --cmd c | source
 # Set GPG TTY - needed for agent to do the right thing
 set -g -x GPG_TTY (tty)
 set -g -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
+gpg-connect-agent updatestartuptty /bye >/dev/null
 
