6c62a6a6d27cac19a510ee5dc95d526134b76087
Frode Egeland <frode@identitii.com>
2021-12-05 08:33:05 +1100

    tweaks
    Signed-off-by: Frode Egeland <frode@identitii.com>

--- shells/fish/functions/ip.fish
+++ shells/fish/functions/ip.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function ip --wraps='dig +short myip.opendns.com @resolver1.opendns.com' --description 'alias ip dig +short myip.opendns.com @resolver1.opendns.com'
-  dig +short myip.opendns.com @resolver1.opendns.com $argv;
+function ip --wraps='curl -4 -s ifconfig.co' --description 'alias ip curl -4 -s ifconfig.co'
+  curl -4 -s ifconfig.co $argv;
 end
