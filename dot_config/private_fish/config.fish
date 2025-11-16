05aae37edef869534ff23ca6789d1f615aa926c9
Frode Egeland <egeland@gmail.com>
2025-11-16 17:16:13 +1100

    add zoxide and lsd config
    Signed-off-by: Frode Egeland <egeland@gmail.com>

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    zoxide init fish | source
+    alias ls='lsd'
+    alias l='ls -l'
+    alias la='ls -a'
+    alias lla='ls -la'
+    alias lt='ls --tree'
 end
