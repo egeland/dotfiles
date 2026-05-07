148cfb44b49330db8d5ecd24dc408f943cc7a425
Frode Egeland <frode.egeland@kudosity.com>
2026-05-07 13:44:03 +1000

    add more files
    Signed-off-by: Frode Egeland <frode.egeland@kudosity.com>

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,15 @@
 if status is-interactive
+    # Load credentials from .env for MCP servers
+    if test -f ~/.env
+        while read -l line
+            if string match -qr '^[A-Z_]+=' $line
+                set -l key (string split -m1 '=' $line)[1]
+                set -l value (string split -m1 '=' $line)[2]
+                set -x $key $value
+            end
+        end < ~/.env
+    end
+
     # Commands to run in interactive sessions can go here
     zoxide init --cmd c fish | source
     alias ls='lsd'
